47e45d0a118e0199959ba2e1e7af7acd8b98a7b5
Abhishek Pillai <abhishek.ap3@gmail.com>
2012-03-25 10:54:58 -0500

    working timestamps on submission index page

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,16 @@
 module SubmissionsHelper
+
+  def timestamp(created_time)
+
+  	time_in_seconds = Time.now - created_time
+
+  	if time_in_seconds < 3601
+  		return "#{(time_in_seconds/60).to_i} minutes"
+  	elsif time_in_seconds < 86401	
+  		return "#{(time_in_seconds/3600).to_i} hours"
+  	else
+  		return "#{(time_in_seconds/86400).to_i} days"
+  	end
+
+  end
 end
